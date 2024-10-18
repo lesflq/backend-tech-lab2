d0fcb11d6b4b3dac8b48617aa5a13b51a114be9b
lesflq <max.09seva@gmail.com>
2024-10-18 19:43:14 +0300

    Refactor mapping in usercontroller

--- src/main/java/uk/sevastianov/lab2/Controllers/UserController.java
+++ src/main/java/uk/sevastianov/lab2/Controllers/UserController.java
@@ -1,15 +1,16 @@
 package uk.sevastianov.lab2.Controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import uk.sevastianov.lab2.Entity.User;
 import uk.sevastianov.lab2.Service.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
@@ -17,24 +18,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
+    @PostMapping("/user")
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<User> getUser(@PathVariable(name = "userId") Long userId) {
         return userService.getUser(userId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping()
+    @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @DeleteMapping("/{userId}")
+    @DeleteMapping("/user/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable(name = "userId") Long userId) {
         if (userService.deleteUser(userId)) {
             return ResponseEntity.noContent().build();
